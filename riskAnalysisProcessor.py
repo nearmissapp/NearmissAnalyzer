bf56383751715782f7d1931b755ecc195f79a1b3
rhdtka21 <59441164+rhdtka21@users.noreply.github.com>
2024-12-05 16:28:07 +0900

    Update riskAnalysisProcessor.py

--- riskAnalysisProcessor.py
+++ riskAnalysisProcessor.py
@@ -1,34 +1,32 @@
 import base64
+import os
 from openai import OpenAI
 from dotenv import load_dotenv
-import os
 from prompts_and_tools import AnalyzeImageRisks, FormatRiskAsJson, RetrieveInformation
 from docx import Document
 
 # .env 파일 로드
 load_dotenv()
 
-class riskAnalysisProcessor:
+
+class RiskAnalysisProcessor:
     def __init__(self):
         """
         클래스 초기화 메서드
-        :param api_key: OpenAI API 키
         """
         self.api_key = os.getenv("OPENAI_API_KEY")
-
+        print(self.api_key)
         if not self.api_key:
             raise ValueError("OPENAI_API_KEY가 .env 파일에 설정되지 않았습니다.")
 
-        # 일반 OpenAI API 
-        # self.client = OpenAI(api_key=self.api_key)
-
-        #OpenRouter API
+        # OpenRouter API
         self.client = OpenAI(
-          base_url="https://openrouter.ai/api/v1",
-          api_key=self.api_key,
+            # base_url="https://openrouter.ai/api/v1", #다시 openai api로 변경됨.
+            api_key=self.api_key
         )
 
-    def encode_image_to_base64(self, image_path):
+    @staticmethod
+    def encode_image_to_base64(image_path):
         """
         이미지 파일을 Base64로 인코딩하는 메서드.
         :param image_path: 인코딩할 이미지 파일 경로
@@ -48,8 +46,7 @@ class riskAnalysisProcessor:
         system_prompt = prompt_manager.get_system_prompt()
         user_prompt = prompt_manager.get_user_prompt()
         image_base64 = self.encode_image_to_base64(image_path)
-        
-        #print(system_prompt, user_prompt)
+
         try:
             response = self.client.chat.completions.create(
                 model="gpt-4o-2024-11-20",
@@ -58,10 +55,7 @@ class riskAnalysisProcessor:
                     {
                         "role": "user",
                         "content": [
-                            {
-                                "type": "text",
-                                "text": user_prompt
-                            },
+                            {"type": "text", "text": user_prompt},
                             {
                                 "type": "image_url",
                                 "image_url": {
@@ -87,25 +81,23 @@ class riskAnalysisProcessor:
         :param analyzed_image_risks: 잠재적 위험 요소 평문 레포트
         :return: JSON 포맷의 OpenAI API 응답
         """
-
         prompt_manager = FormatRiskAsJson()
         system_prompt = prompt_manager.get_system_prompt()
         user_prompt = prompt_manager.get_user_prompt(analyzed_image_risks)
         tools = [prompt_manager.get_tool()]
-        
-        #print(system_prompt, user_prompt, tools)
+
         try:
             # 메시지 정의
             messages = [
                 {"role": "system", "content": system_prompt},
                 {"role": "user", "content": user_prompt},
             ]
-    
+
             # OpenRouter API 호출
             response = self.client.chat.completions.create(
                 model="gpt-4o-2024-11-20",
                 messages=messages,
-                tools=tools,  # tools 필드 추가 
+                tools=tools,  # tools 필드 추가
                 tool_choice=tools[0],  # 도구 호출 강제
                 temperature=0.3,
                 max_tokens=16000,
@@ -113,49 +105,43 @@ class riskAnalysisProcessor:
                 frequency_penalty=0,
                 presence_penalty=0
             )
-    
+
             # 결과 반환
             return response
         except Exception as e:
             return {"error": str(e)}
 
+    def retrieve_information(self, formatted_risks_json, doc_search_keyword):
+        """
+        JSON으로 구성된 잠재적 위험 요소 분석 결과에서 관련 담당자 정보를 검색하고 출력하는 메서드.
+        :param formatted_risks_json: JSON 형식의 위험 요소 분석 결과
+        :param doc_search_keyword: 검색할 문서 키워드
+        :return: OpenAI API 응답
+        """
 
-
-    def retrieve_information(self, formatted_risks_json, docSearchKeyword):
         def read_docx(file_path):
             try:
                 # 파일 불러오기
                 doc = Document(file_path)
-                
+
                 # 모든 문단(paragraphs) 읽기
-                full_text = []
-                for para in doc.paragraphs:
-                    full_text.append(para.text)
-                
+                full_text = [para.text for para in doc.paragraphs]
+
                 return '\n'.join(full_text)
             except Exception as e:
                 return f"오류 발생: {e}"
-        # print(formatted_risks_json, docSearchKeyword)
-        """
-        JSON으로 구성된 잠재적 위험 요소 분석 결과에서 관련 담당자 정보를 검색하고 출력하는 메서드.
-        :param formatted_risks_json: JSON 형식의 위험 요소 분석 결과
-        :return: OpenAI API 응답
-        """        
-        # docSearchKeyword를 사용하여 문서 읽기
+
         try:
-            doc_text = read_docx(f"documents/니어미스 사례집 _ {docSearchKeyword}.docx")
-           
+            doc_text = read_docx(f"documents/니어미스 사례집 _ {doc_search_keyword}.docx")
         except Exception as e:
             print(f"문서 읽기 중 오류 발생: {str(e)}")
-            doc_text = ""
             return {"error": f"문서 읽기 중 오류 발생: {str(e)}"}
-        
+
         prompt_manager = RetrieveInformation()
         system_prompt = prompt_manager.get_system_prompt()
-        user_prompt = prompt_manager.get_user_prompt(docSearchKeyword, doc_text, formatted_risks_json)
+        user_prompt = prompt_manager.get_user_prompt(doc_search_keyword, doc_text, formatted_risks_json)
         tools = [prompt_manager.get_tool()]
-        
-        #print(system_prompt, user_prompt, tools)
+
         try:
             response = self.client.chat.completions.create(
                 model="gpt-4o-2024-11-20",
